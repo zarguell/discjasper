81e9456f6dbebe17387c7fc0b85c02308578e972
Davidson Girard <davidson@girardwebdesign.com>
2014-07-26 02:16:09 -0400

    inital commit, creates socket for user and streams twitter data to node

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+var server = app.listen(3000);
+var io = require('socket.io').listen(server);
+exports.io = io;
+
+console.log('app started on port ' + server.address().port);
+
+//Configuration
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.logger());
+
+app.all('*', function(req, res, next) {
+  	res.set('Access-Control-Allow-Origin', '*');
+  	res.set('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT');
+  	res.set('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
+  	if ('OPTIONS' == req.method) return res.send(200);
+  	next();
+});
+
+var twitter = require('./twitter');
+
---
+++ config.js
@@ -0,0 +1,9 @@
+var config = {}
+
+config.twitter_account = '';
+config.twitter_api_key = '';
+config.twitter_api_secret = '';
+config.twitter_access_token = '';
+config.twitter_access_token_secret = '';
+
+module.exports = config;
