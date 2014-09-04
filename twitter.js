b25adae82aa751aa6cf8f0ddf65e26424860f3f2
Davidson Girard <davidson@girardwebdesign.com>
2014-09-04 17:56:57 -0400

    two arrays one with default songs and one with requests, video also full width

--- twitter.js
+++ twitter.js
@@ -5,21 +5,27 @@ var isFirstConnectionToTwitter = true;
 var FeedParser = require('feedparser');
 var http = require('http');
 
+var requested_list = [];
+var default_list = [{id: "GVFWai1jVfs", time: "43"}, {id: "TbWKGO73Kds", time: "49"}];
+
+//Connection to twitter
 var T = new Twit({
     consumer_key: config.twitter_api_key,
     consumer_secret: config.twitter_api_secret,
     access_token: config.twitter_access_token,        
     access_token_secret: config.twitter_access_token_secret 
 });
-
+// Initializes and starts twitter stream
 var stream = T.stream('user', {track: config.twitter_account });
-stream.start();
 
+//Connection to the browser
 io.sockets.on('connection', function (socket) {
-  console.log("connnecting to comp, starting stream");
+
+  sendDefault();
 
   socket.on('disconnect', function (socket) {
     console.log("disconnect");
+    stream.stop();
   });
 
   stream.on('tweet', function(tweet) {
@@ -31,9 +37,9 @@ io.sockets.on('connection', function (socket) {
         .on('readable', function(){
           var stream = this, item;
           while (item = stream.read()){
-            var msg = item.guid.substr(item.guid.lastIndexOf(":") + 1);
-            console.log(msg);
-            socket.emit("pong", {txt: msg });
+            var vID = item.guid.substr(item.guid.lastIndexOf(":") + 1);
+            var vTime = item['media:group']['yt:duration']['@'].seconds;
+            requested_list.push({id: vID, time: vTime});
           }
         })
       });
@@ -43,10 +49,23 @@ io.sockets.on('connection', function (socket) {
     }
   });
 
-  socket.emit("pong",{txt:"Connected to server"});
-
-  socket.on('ping', function (data) {
-    console.log(data.txt);
+  socket.on('next_song', function (data) {
+    if (requested_list.length > 0) {
+      sendRequested();
+    } else {
+      sendDefault();
+    }
   });
+
+  function sendDefault() {
+    var playNow = default_list.shift();
+    socket.emit('next_song', playNow);
+    default_list.push(playNow);
+  }
+
+  function sendRequested() {
+    socket.emit('next_song', requested_list.shift());
+  }
+
 
 });
