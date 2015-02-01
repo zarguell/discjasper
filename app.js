e12f62205f1bd17098927b7f1832fafd26c3bbe7
Davidson Girard <davidson@girardwebdesign.com>
2015-02-01 14:59:36 -0500

    update entire project to have correct two space tabing

--- app.js
+++ app.js
@@ -15,15 +15,15 @@ app.use(express.static(__dirname + '/public'));
 app.set("view options", {layout: false});
 
 app.get('/', function(req, res) {
-    res.render('index.html');
+  res.render('index.html');
 });
 
 app.all('*', function(req, res, next) {
-  	res.set('Access-Control-Allow-Origin', '*');
-  	res.set('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT');
-  	res.set('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
-  	if ('OPTIONS' == req.method) return res.send(200);
-  	next();
+  res.set('Access-Control-Allow-Origin', '*');
+  res.set('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT');
+  res.set('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
+  if ('OPTIONS' == req.method) return res.send(200);
+  next();
 });
 
 var twitter = require('./twitter');
