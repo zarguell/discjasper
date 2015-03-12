5f191142cb8dc8af29ffa687801ceb14918cb995
Davidson Girard <davidson@girardwebdesign.com>
2015-03-12 17:05:00 -0400

    prepend twitter to all env vars

--- config2.js
+++ config2.js
@@ -3,7 +3,7 @@ var config = {}
 config.twitter_account = process.env.TWITTER_ACCOUNT; 
 config.twitter_api_key = process.env.TWITTER_API_KEY; 
 config.twitter_api_secret = process.env.TWITTER_API_SECRET; 
-config.twitter_access_token = process.env.ACCESS_TOKEN;
-config.twitter_access_token_secret = process.env.ACCESS_TOKEN_SECRET; 
+config.twitter_access_token = process.env.TWITTER_ACCESS_TOKEN;
+config.twitter_access_token_secret = process.env.TWITTER_ACCESS_TOKEN_SECRET; 
 
 module.exports = config;
