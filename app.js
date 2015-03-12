762c5a0bdb426fe9c76b976d9de44a5c04e6eb65
Davidson Girard <davidson@girardwebdesign.com>
2015-03-12 17:23:08 -0400

    switched to standard port 5000 for node js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var server = app.listen(3000);
+var server = app.listen(5000);
 
 var config = require('./config2')
 exports.config = config;
