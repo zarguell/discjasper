e6b2eacda9584c2de58b3fe8f6ceaee4fc86b349
Davidson Girard <davidson@girardwebdesign.com>
2015-04-30 15:48:58 -0400

    change youtube to an add on that can handle the volume

--- youtube.js
+++ youtube.js
@@ -1,10 +1,20 @@
 var http = require('./app').http;
-var request = require("request");
+var youtube = require("youtube-api"); 
+
+youtube.authenticate({
+    type: "key"
+  , key: "AIzaSyAG6jogG2fdEECyDblxs0hMrdUDbGy0Xp4"
+});
 
 exports.search = function(songName, dataFun) {
   songName = songName.split(" ").join("+");
-  request("https://www.googleapis.com/youtube/v3/search?part=snippet&q=" + songName + "&type=video&key=AIzaSyC7RtiPFhHG00A-MPTpL71G6cNy4D5TRfQ", function(error, response, body) {
-    var data = JSON.parse(body);
-    dataFun({id: data["items"][0]["id"]["videoId"],  title: data["items"][0]["snippet"]["title"]});
+  youtube.search.list({
+      "part": "id, snippet",
+      "maxResults": 1,
+      "q": songName 
+  }, function (err, data) {
+      console.log(data);
+      if (!err && data["items"].length > 0)
+        dataFun({id: data["items"][0]["id"]["videoId"], title: data["items"][0]["snippet"]["title"]});
   });
 }
