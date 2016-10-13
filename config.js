54496128ffc3463efb081e239050ca7e4db3dde6
Davidson Girard <davidsongirard@users.noreply.github.com>
2016-10-13 19:22:26 -0400

    remove config

--- config.js
+++
@@ -1,9 +0,0 @@
-var config = {}
-
-config.twitter_account = '';
-config.twitter_api_key = '';
-config.twitter_api_secret = '';
-config.twitter_access_token = '';
-config.twitter_access_token_secret = '';
-
-module.exports = config;
